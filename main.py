c1b9c5c0018f80e4cb35adbd69f9ccf94cad40ee
Xie Zhihao <xzh1206@gmail.com>
2020-12-01 16:17:48 +0800

    Create database if not exists

--- main.py
+++ main.py
@@ -11,12 +11,19 @@ def get_db():
     db = getattr(g, '_database', None)
     if db is None:
         db = g._database = sqlite3.connect(DATABASE)
+
+        c = db.cursor()
+        c.execute('''CREATE TABLE IF NOT EXISTS users(
+            name    TEXT PRIMARY KEY NOT NULL UNIQUE,
+            address TEXT NOT NULL
+            );''')
     return db
 
 
 def get_live_addr(name):
     c = get_db().cursor()
-    cursor = c.execute("SELECT address FROM users WHERE name = '" + name + "'")
+    cursor = c.execute(
+        "SELECT address FROM users WHERE name = '{}'".format(name))
 
     if cursor.rowcount == 0:
         return None
